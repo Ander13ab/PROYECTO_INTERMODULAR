45b0c775d24a33eb59f3c36ef971cc3e63275421
Ander13ab <andergol31@gmail.com>
2026-05-03 23:29:23 +0200

    Configuración backend con Spring Boot

---
+++ backend/src/test/java/com/hazelgym/HazelGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazelgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HazelGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
